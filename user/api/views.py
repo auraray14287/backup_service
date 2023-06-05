d02e52b8ccc438539468bb5cec2aae890613d768
auraray14287 <133588725+auraray14287@users.noreply.github.com>
2023-06-05 14:59:52 +0000

    Delete user directory

--- user/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from user.api.serializers import BackedUpFileSerializer
-from user.models import BackedUpFile
-
-class BackedUpFileViewSet(viewsets.ModelViewSet):
-    queryset = BackedUpFile.objects.all()
-    serializer_class = BackedUpFileSerializer
-    # Implement additional permissions and filtering as needed
